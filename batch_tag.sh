4771d2c393f1e66ebe6d4b52a5534307a9e275a8
Saulo Pereira <saulocr.pereira@gmail.com>
2020-05-26 15:26:18 -0400

    tag: fix tagging logic
    tag the latest commit before a release from sct

--- batch_tag.sh
+++ batch_tag.sh
@@ -3,40 +3,52 @@ WS_PATH=/home/saulo/Work/exmakhina/poly/workspace
 SCT_PATH=/home/saulo/Work/exmakhina/poly/sct
 REPO_PATH=/home/saulo/Work/exmakhina/poly/SCT-data
 
-###################################################################
-# #clear tmp/
-# rm $WS_PATH/tmp/*
+rm $WS_PATH/tmp/*
 
-# cd $SCT_PATH
-# git checkout master
+#### list sct repo tags
+tags_file=$WS_PATH/tmp/tag_sorted.txt
+cd $SCT_PATH
+git checkout master
 
-# git tag > $WS_PATH/tag_list.txt
-# while read TAG;
-# do
-# # DATE=$(git log -1 --format=%ai $TAG)
-# DATE=$(git log -1 --format=%cd --date=short $TAG)
-
-# echo "${DATE} ${TAG}" >> $WS_PATH/tmp/tag_dated.txt
-# done < $WS_PATH/tag_list.txt
-
-# sort $WS_PATH/tmp/tag_dated.txt > $WS_PATH/tmp/tag_sorted.txt
-# tac $WS_PATH/tmp/tag_sorted.txt > $WS_PATH/tag_sorted.txt
-
-
-###################################################################
-while read DATE_TAG;
+while read TAG;
 do
-dt=($DATE_TAG) #array
-date_str=${dt[0]}
-tag=${dt[1]}
+	DATE=$(git log -1 --format=%cd --date=short $TAG)
+	echo "${DATE} ${TAG}" >> $WS_PATH/tmp/tag_dated.txt
+done < <(git tag)
 
-arg_date=$(date -d ${1} +%s)
-tag_date=$(date -d ${date_str} +%s)
-if [ $arg_date -ge $tag_date ];
-then
-	echo $tag
-	exit
-fi
-done < $WS_PATH/tag_sorted.txt
+#sort tags based on date
+sort -r $WS_PATH/tmp/tag_dated.txt > $tags_file
 
-echo "Untagged"
+#### apply sct tags to corresponding data commits
+cd $REPO_PATH
+
+while read SHA;
+do
+	commit_msg=$(git log -1 $SHA --pretty="%s")
+	commit_date=${commit_msg:0:10}
+
+	re='^[0-9]+$'
+	if ! [[ ${commit_date:0:3} =~ $re ]] ; then
+		continue
+	fi
+
+	tag_str="--"
+	while read DATE_TAG;
+	do
+		following_release=$tag_str
+
+		dt=($DATE_TAG)
+		date_str=${dt[0]}
+		tag_str=${dt[1]}
+
+		com_date=$(date -d $commit_date +%s)
+		tag_date=$(date -d $date_str +%s)
+		if [ $com_date -ge $tag_date ];
+		then
+			if ! [ $following_release = "--" ]; then
+				git tag $following_release $SHA
+			fi
+			break
+		fi
+	done < $tags_file
+done < <(git log --pretty=%h)
