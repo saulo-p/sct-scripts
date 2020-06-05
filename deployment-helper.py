9acdf7ab8489ac83ae8c4649ec0f01195e164f65
Saulo Pereira <saulocr.pereira@gmail.com>
2020-06-05 15:17:13 -0400

    create_release: create a release to the target repository
    bases the release on a given tag

--- deployment-helper.py
+++ deployment-helper.py
@@ -1,12 +1,36 @@
 import argparse
+import argparse, json, os
 import logging
+import urllib.request, urllib.error
 
 logger = logging.getLogger()
+
 sct_data_repos = ['sct_testing_data', 'sct_example_data', 'deepseg_lesion_models', 'deepseg_sc_models', 'deepseg_gm_models', 'pmj_models', 'optic_models', 'c2c3_disc_models', 'MNI-Poly-AMU', 'PAM50']
+GH_TOKEN=os.environ["GH_TOKEN"].strip()
+
 
 def create_release(target_repo, target_tag):
-	'''Create a new release within the target repository.'''
-	pass
+	'''
+	Create a new release within the target repository.
+	:return: release id on success.'''
+	logger.info("Creating release")
+
+	url = "https://api.github.com/repos/sct-data/{}/releases".format(target_repo)
+	headers = {
+		"Authorization": "token {}".format(GH_TOKEN),
+		"Content-Type": "application/json",
+	}
+	payload = json.dumps({"tag_name": target_tag, "name": target_tag, "draft": False, "prerelease": False}).encode("utf-8")
+	req = urllib.request.Request(url, headers=headers, method="POST", data=payload)
+
+	with urllib.request.urlopen(req) as resp:
+		if resp.getcode() != 201:
+			raise RuntimeError("Bad response: %d / %s", resp.getcode(), resp.read())
+		ret = json.loads(resp.read().decode('utf-8'))
+		logger.info("ret: %s", ret)
+		release_id = ret["id"]
+
+	return release_id
 
 def download_default_release_asset(target_repo, release_id):
 	'''Download the default asset of a given release'''
