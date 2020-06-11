0f45413410bacb7d3aec44e3870cb5ee599217af
Saulo Pereira <saulocr.pereira@gmail.com>
2020-06-11 11:19:35 -0400

    osf upload: implement with osfclient api instead of subprocess
    also, returns the download link

--- deployment-helper.py
+++ deployment-helper.py
@@ -81,23 +81,40 @@ def upload_github_asset(target_repo, release_id, asset_path):
 		ret = json.loads(resp.read().decode('utf-8'))
 		logger.info("ret: %s", ret)
 
-def upload_to_osf(osf_project, asset_path):
+def upload_to_osf(osf_project_id, asset_path, upload_path):
 	'''
 	Uploads new version of the data to the Open Science Framework.
-	:return: osf url of the newly uploaded asset.
+	:return: osf download url of the newly uploaded asset.
 	'''
-	import subprocess
+	from osfclient import OSF
+
+	#TODO: move this logic to argparsing (as alternative to cli arguments)
 	try:
 		os.environ['OSF_USERNAME']
 		os.environ['OSF_PASSWORD']
 	except:
-		print("Please provide OSF environment variables")
-		raise SystemExit()
+		raise SystemExit("Please provide OSF environment variables")
 
-	cmd = ['osf', '-u', os.environ['OSF_USERNAME'], '-p', osf_project, 'upload', asset_path, 'data/'+asset_path]
-	subprocess.run(cmd)
-	return
+	osf = OSF()
+	osf.login(username=os.environ['OSF_USERNAME'], password=os.environ['OSF_PASSWORD'])
 
+	project_storage = osf.project(osf_project_id).storage()
+
+	logger.info('Uploading file to OSF project [%s]', osf_project_id)
+	with open(asset_path, 'rb') as fup:
+		try:
+			project_storage.create_file(upload_path, fup)
+		except Exception as e:
+			logger.error('Upload error: %s', e)
+		else:
+			for file in project_storage.files:
+				# compare strings instead of os.path.samefile() because objects are in different file systems.
+				if file.path == upload_path:
+					return file._download_url
+
+def update_release_(target_repo, release_id, osf_url):
+	'''
+	Include link to osf data in the github release
 	'''
 	pass
 
